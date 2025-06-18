0c30b4c7456d886990095fec9ca4515d93f4348b
pchound <chaonerdguy@gmail.com>
2025-06-18 17:45:06 -0600

    my-orders3

--- app/api/order/list/route.js
+++ app/api/order/list/route.js
@@ -1,17 +1,21 @@
-import connectDB from "@/config/db"
-import Address from "@/models/Address"
-import Order from "@/models/Order"
-import Product from "@/models/Product"
-import { getAuth } from "@clerk/nextjs/dist/types/server"
-import { NextResponse } from "next/server"
+import connectDB from "@/config/db";
+import Address from "@/models/Address";
+import Order from "@/models/Order";
+import Product from "@/models/Product";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
 
 export async function GET(request) {
     try {
+
         const { userId } = getAuth(request)
+
         await connectDB()
 
-        Address.length
-        Product.length
+        await Address.length
+        await Product.length
 
         const orders = await Order.find({ userId }).populate('address items.product')
 
