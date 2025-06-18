deed9715f64f42c5df748882e90f2ea741f0b84e
pchound <chaonerdguy@gmail.com>
2025-06-18 16:32:53 -0600

    can-we-order

--- app/api/user/get-address/route.js
+++ app/api/user/get-address/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
     try {
-        const { userId } = getAuth
+        const { userId } = getAuth(request)
         await connectDB()
         const addresses = await Address.find({ userId })
         return NextResponse.json({ success: true, addresses });
--- config/inngest.js
+++ config/inngest.js
@@ -56,3 +56,32 @@ export const syncUserDeletion = inngest.createFunction(
         await User.findByIdAndDelete(id)
     }
 )
+
+
+// Inngest Function to create user's order in database
+export const createUserOrder = inngest.createFunction(
+    {
+        id: 'create-user-order',
+        batchEvents: {
+            maxSize: 25,
+            timeout: '5s'
+        }
+    },
+    { event: 'order/created' },
+    async ({ events }) => {
+        const orders = events.map((event) => {
+            return {
+                userId: event.data.userId,
+                items: event.data.items,
+                amount: event.data.amount,
+                address: event.data.address,
+                date: event.data.date
+            }
+        })
+        await connectDB()
+        await Order.insertManyorders()
+
+        return { success: true, processed: orders.length };
+    }
+
+)
